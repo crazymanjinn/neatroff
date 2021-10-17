c23c9328ff1aa17920f978ecdd35d99bebf6b3b6
crazymanjinn <crazymanjinn@users.noreply.github.com>
2021-10-17 17:07:59 -0400

    Version neatroff meta-package same as roff package

--- neatroff.nix
+++ neatroff.nix
@@ -3,5 +3,8 @@
   name = "neatroff";
   paths = (lib.attrValues subPackages);
 }).overrideAttrs (oldAttrs: {
+  pname = oldAttrs.name;
+  name = "${oldAttrs.name}-${version}";
+  inherit version;
   meta.mainProgram = "roff";
 })
